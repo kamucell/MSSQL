466ee9296a645d5faf989b3621cbf670c53d15a1
kamucell <kamuranseckin@hotmail.com>
2023-03-03 15:03:17 +0300

    Update 1) INFORMATION_SCHEMA.COLUMNS.sql

--- System Tables/1) INFORMATION_SCHEMA.COLUMNS.sql
+++ System Tables/1) INFORMATION_SCHEMA.COLUMNS.sql
@@ -4,10 +4,10 @@ select * from INFORMATION_SCHEMA.COLUMNS
 
 SELECT     [COLUMN_NAME]  FROM    INFORMATION_SCHEMA.COLUMNS  WHERE    ORDINAL_POSITION=1
 
---The bellow queqy retries columns name  whichs are first rows in the tables of database.
+--The below query show name list which are first columns in the tables of database..
 
 
 SELECT     [COLUMN_NAME]  FROM    INFORMATION_SCHEMA.COLUMNS  WHERE TABLE_NAME = 'tableName' and    ORDINAL_POSITION=1
 
---The bellow queqy retries columns name  which is first rows in the spesific tables.
+--The below query show column name which is first column of specific table
 
